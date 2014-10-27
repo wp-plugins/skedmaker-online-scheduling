4c8c84ef5a82e9a66bb16a24a87b568e7889873e
theArab <theArab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-27 00:13:05 +0000

    edit version in wp-skedmaker.php
    git-svn-id: https://plugins.svn.wordpress.org/skedmaker-online-scheduling/trunk@1014413 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-skedmaker.php
+++ wp-skedmaker.php
@@ -14,8 +14,6 @@ Author URI: Corey Kingsbury
 add_action('activated_plugin','save_error');
 function save_error(){file_put_contents(plugin_dir_path( __FILE__ ) . '/error.html', ob_get_contents());}
 
-//http://codex.wordpress.org/Function_Reference/add_menu_page
-
 function plugin_admin_add_page() {
 	add_menu_page( 'Skedmaker Admin', 'Skedmaker', 'manage_options', 'skedmaker/admin_home.php', '', 'dashicons-calendar-alt', 81);
 }
@@ -23,20 +21,14 @@ add_action('admin_menu', 'plugin_admin_add_page');
 
 
 function my_enqueue($hook) {
-  //-- for our special admin page
+  //-- admin page
 	if( 'skedmaker/admin_home.php' != $hook )
 		return;
 
   wp_register_style('skedmaker', plugins_url('skedmaker/_includes/sm-styles.php'));
   wp_enqueue_style('skedmaker');
 
-// wp_enqueue_script('pluginscript', plugins_url('skedmaker/includes/sm-javascript.php', __FILE__ ), array('jquery'));
 }
-
-//--- WP JQUERY
-//function SM_inc_jquery() {wp_enqueue_script('jquery');}
-//add_action('init', 'SM_inc_jquery');
-
 
 //-- Start session if not already started
 function register_session(){if( !session_id() )session_start();}
