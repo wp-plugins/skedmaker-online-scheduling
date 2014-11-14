c9ad0b59809ad709e5b5bd821c2956daf8b94574
theArab <theArab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-14 13:16:30 +0000

    function update + fix missing graphic on admin scheduling
    git-svn-id: https://plugins.svn.wordpress.org/skedmaker-online-scheduling/tags/.90@1025744 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- _admin/_adminsked.php
+++ _admin/_adminsked.php
@@ -10,6 +10,6 @@
 <?php SM_menu();?>
 <table class='cc100'>
 <tr><td class='pad7'><div class='navMenuRound' style='width:550px;'><a href="<?php echo $smadmin;?>&amp;v=blocktime&amp;ts=<?php echo $_GET['ts'];?>&amp;" class='sked'><img src='<?php echo $sm_btns_dir;?>btn_block32_reg.png' class='btn'>Block Time Frame: <?php echo SM_apt($_GET['ts']);?></a></div></td></tr>
-<tr><td class='pad7'><div class='navMenuRound' style='width:575px;'><a href="<?php echo $smadmin;?>&amp;v=skedclient&amp;ts=<?php echo $_GET['ts'];?>&amp;" class='sked'><img src='<?php echo $sm_btns_dir;?>btn_people32_reg.png' class='btn'>Schedule Someone for: <?php echo SM_apt($_GET['ts']);?></a></div></td></tr>
+<tr><td class='pad7'><div class='navMenuRound' style='width:575px;'><a href="<?php echo $smadmin;?>&amp;v=skedclient&amp;ts=<?php echo $_GET['ts'];?>&amp;" class='sked'><img src='<?php echo $sm_btns_dir;?>btn_clients32_reg.png' class='btn'>Schedule Someone for: <?php echo SM_apt($_GET['ts']);?></a></div></td></tr>
 </table>
 </td></tr></table>
