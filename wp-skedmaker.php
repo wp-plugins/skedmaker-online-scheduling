4d5bf2aad989dcbbfabe4f9be436e1084033a7ab
theArab <theArab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-20 15:02:14 +0000

    v .95
    git-svn-id: https://plugins.svn.wordpress.org/skedmaker-online-scheduling/trunk@1071758 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-skedmaker.php
+++ wp-skedmaker.php
@@ -7,7 +7,7 @@
 Plugin Name: Skedmaker
 Plugin URI: http://www.skedmaker.com/?op=skedmaker&amp;
 Description: Online Appointment Scheduling
-Version: 0.94
+Version: 0.95
 Author URI: http://www.skedmaker.com/
 */
 
